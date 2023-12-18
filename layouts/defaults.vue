2116317f90a1aa92d208cbddb9d734ba6d8ba483
Matt Crandell <mrcrandell@gmail.com>
2023-12-17 20:46:38 -0500

    updated deploy script

---
+++ layouts/defaults.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+@use '~/assets/scss/fonts';
+@use '~/assets/scss/reboot';
+// @import '~/assets/scss/_typography.scss';
+// @import '~/assets/scss/_modals.scss';
+// @import '~/assets/scss/_global.scss';
+
+</style>
